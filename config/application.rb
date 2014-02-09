b7216fc04f093fa8d6f08620e6de7ff67c999987
RohitRox <link2j.roy@gmail.com>
2014-02-09 14:49:01 +0545

    initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Hamrochitwan
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.digest = true
